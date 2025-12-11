1a203c018f3ba2878576c5c152791ee8ee87846f
Farjad Malik <m.farjad.malik@gmail.com>
2025-12-11 16:42:49 +0500

    Solved d11

--- src/2026/d11/main.py
+++ src/2026/d11/main.py
@@ -1,18 +1,45 @@
+import networkx as nx
+
+from functools import cache
+
 # Internal imports
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
 
 
-def part_1() -> int:
+def parse_raw(data: str):
     """
+    Docstring for parse_raw
+    
+    :param data: Raw data from the input file
+    :type data: str
     """
-    ...
+    lines = [line.partition(": ") for line in data.splitlines()]
+    # logger.debug(f"lines - {lines}")
+    network = nx.DiGraph({node: neighbors.split() for node, _, neighbors in lines})
+    return network
 
-def part_2() -> int:
+def part_1(network : nx.DiGraph) -> int:
     """
     """
-    ...
+    # logger.debug(f"All simple paths: \n{list(nx.all_simple_paths(network, "you", "out"))}")
+    return len(list(nx.all_simple_paths(network, "you", "out")))
+
+def part_2(graph: nx.DiGraph) -> int:
+    """ 
+    Count unique paths from start_node to "out" using recursion and memoization.
+    """
+
+    @cache
+    def _count(current: str, seen) -> int:
+        """ Inner function used because the graph itself is unhashable. """
+        if current == "out":
+            return seen == 2
+        seen += current in {"fft", "dac"}
+        return sum(_count(neighbor, seen) for neighbor in graph[current])
+    
+    return _count("svr", 0)
 
 def main(fp_input: str) -> None:
     """
@@ -24,16 +51,18 @@ def main(fp_input: str) -> None:
     # Read input file content 
     with open(fp_input, "r", encoding="utf-8") as f:
         content = f.read()
-    logger.debug(f"File content - \n{content}")
-    # data = [row for row in content.splitlines()]  
+    device_network = parse_raw(content)
+    logger.debug(f"File content - \n{device_network}")
 
-    result = part_1()
-    logger.info(f"Solved for {fp_input}, use (part 1): {result}")
 
-    result = part_2()
+    # result = part_1(device_network)
+    # logger.info(f"Solved for {fp_input}, use (part 1): {result}")
+
+    result = part_2(device_network)
     logger.info(f"Solved for {fp_input}, use (part 2): {result}")
 
 
 if __name__ == "__main__":
-    main(fp_input="src/2026/d11/test.txt")
-    # main(fp_input="src/2026/d11/input.txt")
+    # main(fp_input="src/2026/d11/test.txt")
+    # main(fp_input="src/2026/d11/test_p2.txt")
+    main(fp_input="src/2026/d11/input.txt")
